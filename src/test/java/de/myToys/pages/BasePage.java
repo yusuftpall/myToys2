ea87a28e63f637005a534135ea211681bc262b04
Sevki <sevki.oksuz@gmail.com>
2022-06-23 23:04:23 +0200

    new project

---
+++ src/test/java/de/myToys/pages/BasePage.java
@@ -0,0 +1,15 @@
+package de.myToys.pages;
+
+import de.myToys.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public abstract class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+}
